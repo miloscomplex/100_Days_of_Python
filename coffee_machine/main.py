9b4f725a2aeecd93d5b782bd2c2c2935f4fe91c1
Michael Sutton <hicallmesutton@gmail.com>
2021-08-16 12:03:25 -0500

    creating CLI coffee app

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -29,3 +29,56 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+
+def is_resource_sufficient(order_ingredients):
+    """Checks if the resources are enough to make the drink"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+def process_coins():
+    """Returns the total amount of money inserted"""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.10
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+
+def is_transactions_sucessful(money_received, drnk_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money Refunded")
+        return False
+
+
+
+profit = 0
+
+is_on = True
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino):")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee:{resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            is_transactions_sucessful(payment, drink["cost"])
