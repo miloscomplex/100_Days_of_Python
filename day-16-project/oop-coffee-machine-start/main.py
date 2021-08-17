79540f384789c0a75d1a13b51e8d08ce8c3b2b7b
Michael Sutton <hicallmesutton@gmail.com>
2021-08-16 21:37:00 -0500

    small change

--- day-16-project/oop-coffee-machine-start/main.py
+++ day-16-project/oop-coffee-machine-start/main.py
@@ -4,7 +4,6 @@ from money_machine import MoneyMachine
 
 
 menu = Menu()
-# menu_item = MenuItem()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
@@ -22,4 +21,4 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink)
+        coffee_maker.make_coffee(drink)
