0dccd68e03b52b1db849e316fe0355f7a28d2409
Michael Sutton <hicallmesutton@gmail.com>
2021-08-17 23:47:32 -0500

    added random rgb color to turtle

--- day-18-start/random_walk.py
+++ day-18-start/random_walk.py
@@ -4,6 +4,7 @@ import random
 from random import randrange
 
 tim = t.Turtle()
+t.colormode(255)
 tim.shape("turtle")
 tim.color("coral")
 tim.pensize(10)
@@ -11,11 +12,19 @@ tim.speed("fastest")
 
 colors = ["cornflower blue","forest green","dark salmon","medium purple","magenta","wheat"]
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r,g,b)
+
+print(random_color())
+
 directions = [0, 90, 180, 270]
 
 for _ in range(300):
-    tim.color(random.choice(colors))
-    tim.forward(30 + randrange(20))
+    tim.color(random_color())
+    tim.forward(30 + random.randint(0, 30))
     tim.setheading(random.choice(directions))
 
 screen = Screen()
