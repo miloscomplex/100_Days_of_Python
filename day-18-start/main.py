e99e3a8d3eff0b723cfe11493393f60b3bfc64fb
Michael Sutton <hicallmesutton@gmail.com>
2021-08-17 22:41:04 -0500

    day-18 start code

---
+++ day-18-start/main.py
@@ -0,0 +1,28 @@
+from turtle import Screen
+import turtle as t
+import random 
+
+tim = t.Turtle()
+tim.shape("turtle")
+tim.color("coral")
+
+colors = ["cornflower blue","forest green","dark salmon","medium purple","yellow","wheat"]
+
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+#
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
+
+screen = Screen()
+screen.exitonclick()
