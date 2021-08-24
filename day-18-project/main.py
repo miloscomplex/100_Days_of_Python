d78545996d4b75b40408fc00e6074f8d0e2c9f52
Michael Sutton <hicallmesutton@gmail.com>
2021-08-23 23:28:31 -0500

    added comment to day 18

--- day-18-project/main.py
+++ day-18-project/main.py
@@ -2,6 +2,7 @@ import random
 from turtle import Screen
 import turtle as t
 
+# generated via script 
 color_list = [(1, 12, 31), (54, 25, 17), (218, 127, 106), (9, 104, 160), (242, 213, 68), (150, 83, 39), (216, 86, 63), (156, 6, 24), (165, 162, 30), (158, 62, 102), (207, 73, 103), (10, 64, 33), (11, 96, 57), (95, 6, 20), (175, 134, 162), (7, 173, 217), (1, 61, 145), (2, 213, 207), (158, 32, 23), (8, 140, 85)]
 
 tim = t.Turtle()
@@ -34,7 +35,7 @@ for _ in range(10):
     tim.setpos(x,y)
     draw_line_of_circles()
     y += 40
-    
+
 
 screen = Screen()
 screen.exitonclick()
