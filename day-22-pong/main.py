29d5200f94ff0df8396d2c9573881dc5a21fadc2
Michael Sutton <hicallmesutton@gmail.com>
2021-09-03 23:41:15 -0500

    project completed pong daay 22

--- day-22-pong/main.py
+++ day-22-pong/main.py
@@ -1,5 +1,8 @@
+import time
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from score_board import ScoreBoard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,6 +12,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -21,6 +26,31 @@ screen.onkey(l_paddle.go_down, "z")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 270 or ball.ycor() < -270:
+        ball.bounce_y()
+
+    #detect collision with r_paddle or l_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+
+    #Detect if r_paddle missed
+    if ball.xcor() > 390:
+        scoreboard.l_point()
+        time.sleep(0.1)
+        ball.reset_position()
+
+    #Detect if l_paddle missed
+    if ball.xcor() < -390:
+        scoreboard.r_point()
+        time.sleep(0.1)
+        ball.reset_position()
+
+    #detect collision with l_paddle
 
 screen.exitonclick()
--- day-22-pong/paddle.py
+++ day-22-pong/paddle.py
@@ -7,7 +7,7 @@ class Paddle(Turtle):
     def __init__(self, tupple):
         super().__init__()
         self.shape("square")
-        self.color("white")
+        self.color("green")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(tupple)
