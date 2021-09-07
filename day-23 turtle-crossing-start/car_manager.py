fee48edc1772112c04c61517e159813ccd2abaf9
Michael Sutton <hicallmesutton@gmail.com>
2021-09-07 16:44:31 -0500

    progress commit for turtle crossing game

--- day-23 turtle-crossing-start/car_manager.py
+++ day-23 turtle-crossing-start/car_manager.py
@@ -1,7 +1,25 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.create_car()
+
+    def create_car(self):
+        self.shape("square")
+        self.color(COLORS[random.randint(0, len(COLORS) - 1)])
+        self.shapesize(stretch_wid=1, stretch_len=3)
+        self.penup()
+        self.goto(random.randint(-270, 270), random.randint(-270, 270))
+
+
+    def move_forward(self):
+        self.goto(self.xcor() - 10, self.ycor())
+        if self.xcor() < -350:
+            self.goto(300, random.randint(-270, 270))
--- day-23 turtle-crossing-start/main.py
+++ day-23 turtle-crossing-start/main.py
@@ -1,14 +1,32 @@
 import time
-from turtle import Screen
+from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+SPEED = 10;
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+car = CarManager()
+
+
+
+def go_up():
+    turtle.forward(SPEED)
+
+turtle = Turtle()
+turtle.penup()
+turtle.shape("turtle")
+turtle.left(90)
+turtle.goto(0,-280)
+
+screen.listen()
+screen.onkey(go_up, "Up")
 
 game_is_on = True
+
 while game_is_on:
     time.sleep(0.1)
+    car.move_forward()
     screen.update()
