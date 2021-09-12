59da38652dc8a36b0ab8a9cfefa14c349de2c878
Michael Sutton <hicallmesutton@gmail.com>
2021-09-11 23:16:09 -0500

    completed day-24 challenge automation of letters

--- day-24-challenge/Mail Merge Project Start/main.py
+++ day-24-challenge/Mail Merge Project Start/main.py
@@ -6,3 +6,17 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+with open("./Input/Names/invited_names.txt") as data:
+    names = data.readlines()
+
+
+print(names)
+
+for name in names:
+    with open("./Input/Letters/starting_letter.txt") as letter:
+        letter_text = letter.read()
+        file = open(f"./Output/ReadyToSend/letter_{name.strip()}.txt", mode="w")
+        file.write(letter_text.replace("[name]", name.strip()))
+        file.close()
