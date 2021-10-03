528d0726d9ab00fb77266e3f4f59f46d68cfb9de
Michael Sutton <hicallmesutton@gmail.com>
2021-10-03 16:43:39 -0500

    start of challenge day 25

--- day-25/start/main.py
+++ day-25/start/main.py
@@ -15,6 +15,24 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+print(type(data))
+# print(data["temp"])
 
-print(data["temp"])
+data_dict = data.to_dict()
+print(data_dict)
 
+# temp_list = data["temp"].to_list()
+# avg = sum(temp_list) / len(temp_list)
+print(data["temp"].mean())
+
+print(data["temp"].max())
+
+print(data[data.day == "Monday"])
+
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+
+print(monday.temp * 1.8 + 32)
+
+pandas.DataFrame(data_dict)
