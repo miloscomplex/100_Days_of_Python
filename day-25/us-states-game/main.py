0198a19747573aee3135e2f30322a9477c8fe1ac
Michael Sutton <hicallmesutton@gmail.com>
2021-10-03 23:13:01 -0500

    progress push challenge 25

--- day-25/us-states-game/main.py
+++ day-25/us-states-game/main.py
@@ -9,16 +9,26 @@ screen.addshape(image)
 turtle.shape(image)
 
 states_data = pandas.read_csv("50_states.csv")
+all_states = states_data.state.to_list()
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Guessed", prompt="What's another state's name?").title()
 
-def correct_guess(guess):
-    if states_data[states_data.state == guess.title()]:
-        print("yup")
-    else:
-        print(guess)
+    if answer_state == "Exit":
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = states_data[states_data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
 
-correct_guess(answer_state)
-
+# states_to_learn.csv
+def states_to_learn():
+    
 turtle.mainloop()
