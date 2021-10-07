6ac67472627867d8bf9cccb496e6395d979b8c89
Michael Sutton <hicallmesutton@gmail.com>
2021-10-06 22:32:19 -0500

    day 26

--- day-26/main.py
+++ day-26/main.py
@@ -8,3 +8,7 @@ print(new_numbers)
 range_list = [num * 2 for num in range(1,5)]
 
 short_names = [name for name in names if len(name) < 5]
+
+# [new_item for item in items if test]
+
+all_caps = [name.upper() for name in names if len(name) > 5]
