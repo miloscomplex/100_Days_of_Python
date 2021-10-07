d58ffc7c7e57ed215bfc3a11e7bfa14003d65cc7
Michael Sutton <hicallmesutton@gmail.com>
2021-10-06 22:28:57 -0500

    day 26 learning about lists

--- day-26/main.py
+++ day-26/main.py
@@ -3,3 +3,8 @@ numbers = [1,2,3]
 new_numbers = [n + 1 for n in numbers]
 
 print(new_numbers)
+
+
+range_list = [num * 2 for num in range(1,5)]
+
+short_names = [name for name in names if len(name) < 5]
